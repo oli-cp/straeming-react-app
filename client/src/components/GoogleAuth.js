7a8e75547ba1ae390e55beeb615dcb040f507f4e
oli-cp <oliver.cpi@gmail.com>
2020-04-08 12:06:05 +0200

    redux forms and StreamCreate component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,8 +3,6 @@ import { connect } from "react-redux";
 import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
-    state = { isSignedIn: null };
-
     componentDidMount() {
         window.gapi.load("client:auth2", () => {
             window.gapi.client.init({
@@ -13,14 +11,20 @@ class GoogleAuth extends React.Component {
             })
                 .then(() => {
                     this.auth = window.gapi.auth2.getAuthInstance();
-                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                    this.onAuthChange(this.auth.isSignedIn.get());
                     this.auth.isSignedIn.listen(this.onAuthChange);
                 });
         });
     }
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    onAuthChange = (isSignedIn) => {
+        const googleUser = this.auth.currentUser.get().getId();
+
+        if (isSignedIn) {
+            this.props.signIn(googleUser);
+        } else {
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -32,9 +36,9 @@ class GoogleAuth extends React.Component {
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
@@ -56,4 +60,8 @@ class GoogleAuth extends React.Component {
     }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
